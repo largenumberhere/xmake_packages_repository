c3f7c3f234a223c495917654ad1be4a06b11a996
largenumberhere <largeNumbergoeshere@gmail.com>
2025-05-04 16:34:22 +1000

    fix

--- packages/h/hashmap_c_library/xmake.lua
+++ packages/h/hashmap_c_library/xmake.lua
@@ -1,11 +1,18 @@
 package("hashmap_c_library")
-    set_kind("binary")
     set_description("The hashmap_c_library package")
+
+    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
 
     on_load(function (package)
         package:set("installdir", path.join(os.scriptdir(), package:plat(), package:arch(), package:mode()))
     end)
 
     on_fetch(function (package)
-        return {program = path.join(package:installdir("bin"), "hashmap_c_library.exe")}
+        local result = {}
+        local libfiledir = (package:config("shared") and package:is_plat("windows", "mingw")) and "bin" or "lib"
+        result.links = "hashmap_c_library"
+        result.linkdirs = package:installdir("lib")
+        result.includedirs = package:installdir("include")
+        result.libfiles = path.join(package:installdir(libfiledir), "hashmap_c_library.lib")
+        return result
     end)
